582f97d708377b3c8cea0c3a45f2c1301d9e27dc
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2024-09-04 22:53:41 +0600

    slice, join, split

---
+++ concat.js
@@ -0,0 +1,5 @@
+const first = 'Mufassir';
+const last = 'Mahmud';
+const full = first.concat(' ').concat(last)
+console.log(full);
+console.log(last.includes('a'));
---
+++ slice.js
@@ -0,0 +1,5 @@
+const name = 'Mufassir';
+console.log(name.slice(0,4));
+
+const sentence = 'Aki rono baja baje ghono ghono';
+console.log(sentence.split('a'));
