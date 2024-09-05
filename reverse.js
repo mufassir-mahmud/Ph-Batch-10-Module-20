c886d1e4966056f763519b028fcaeedc9714721e
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2024-09-05 11:56:07 +0600

    reverse

---
+++ reverse.js
@@ -0,0 +1,13 @@
+const sentence = 'I am a web developer';
+let reverse = '';
+// for(const letter of sentence){
+//     reverse = letter + reverse;
+// }
+// console.log(reverse);
+
+// for(let i = 0; i < sentence.length; i++){
+//     reverse = sentence[i] + reverse;
+// }
+// console.log(reverse);
+// const reserved = sentence.split('').reverse().join('');
+// console.log(reserved);
