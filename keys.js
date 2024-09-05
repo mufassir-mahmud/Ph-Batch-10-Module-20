a32783e976c892c09e17a80f452e1c38a7b03087
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2024-09-05 18:48:11 +0600

    added keys

---
+++ keys.js
@@ -0,0 +1,11 @@
+const computer = {
+    name: 'walton',
+    price: 43000,
+    monitor : 'cined',
+    hdd: 1024,
+    
+}
+const keys = Object.keys(computer);
+console.log(keys);
+const values = Object.values(computer);
+console.log(values);
---
+++ property.js
@@ -0,0 +1,13 @@
+const person ={
+    firstName: 'Mufassir',
+    lastName: 'Mahmud',
+    age: 23,
+    study: 'cse',
+    varsity: 'SEU'
+}
+person.age = 22;
+person['varsity'] = 'NSU'
+console.log(person);
+// console.log(person.firstName);
+// const boyosh = person['age'];
+// console.log(boyosh);
